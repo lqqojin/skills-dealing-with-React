d5af041eae2a6ecac4a81dcfc1ce50eec5cba4ce
lqqojin <lqqojin@gmail.com>
2020-09-08 17:58:28 +0900

    feat: ch04 step01 이벤트  S01EventPractice 렌더링

---
+++ src/ch04/S01EventPractice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class S01EventPractice extends Component {
+	render() {
+		return (
+			<div>
+				<h1>이벤트 연습</h1>
+			</div>
+		)
+	}
+}
+
+export default S01EventPractice;
